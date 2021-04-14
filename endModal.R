b1c1d9580be26d2b9e6d9e7859d5494b2a36dcc4
Chung Wei Lin <weils@Chungs-MacBook-Air.local>
2021-04-14 13:02:31 +0800

    updated endModal function to return names and scores

--- endModal.R
+++ endModal.R
@@ -1,9 +1,23 @@
 #outputs end of modal
 endModal <- function(p1Score, p1Name, p2Score, p2Name){
+  winner <- c(p1Name,p2Name)[which.max(c(p1Score, p2Score))]
+  loser <- c(p1Name,p2Name)[which.min(c(p1Score, p2Score))]
   modal <- fluidPage(
-    tags$h4(paste0(p1Name, " wins with ", p1Score, " points!")), 
+    tags$h4(paste0(winner, " wins with ", p1Score, " points!")), 
     tags$hr(),
-    tags$h5(paste0(p2Name, " loses with ", p2Score, " points"))
+    tags$h5(paste0(loser, " loses with ", p2Score, " points"))
   ) 
   return(modal)
 }
+
+
+# testing
+# getscores <- function(p1Score, p1Name, p2Score, p2Name){
+#   winner <- c(p1Name,p2Name)[which.max(c(p1Score, p2Score))]
+#   loser <- c(p1Name,p2Name)[which.min(c(p1Score, p2Score))]
+#   paste("the winner is", winner, "with", p1Score, "points, and the loser is", loser, "with", p2Score, "points")
+# 
+#   
+# }
+# 
+# getscores(10, "player1", 3, "player2")
