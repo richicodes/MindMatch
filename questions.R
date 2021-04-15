ffc66f0e6633a2fd9cc2d6a99053218c33d8e45e
SarahRamjoo <63096820+SarahRamjoo@users.noreply.github.com>
2021-04-15 23:31:18 +0800

    questions.R

--- questions.R
+++ questions.R
@@ -1,20 +1,59 @@
 # ## questions.R
-# Functions related to retrieval of questiions and answers
+# Functions related to retrieval of questions and answers
 # ### questionRetrieve(answered_questions)
-# Function will retrieve question and answers not listed in the game, and also returns updated list of answered quesrtions
+# Function will retrieve question and answers not listed in the game, and also returns updated list of answered questions
 # #### input:
 # answered_question contains list of questionIDs already displayed in the game.
 # #### output:
 # list(questionID, question, options, correct_option_index, answered_questions_updated)
 
-DRAFTquestionRetrieve <- function(answered_questions){
-  return(
-    list(
-      "questionID" = 1,
-      "question" = "TEST QUESTION",
-      "options" = c("option1", "option2", "option3", "option4"),
-      "correct_option_index" = 1, #if option 1 is correct, etc
-      "answered_questions_updated" = c(1, 2, 3, 4, 5) #updated list of answered questions
-    )
-  )
+library(DBI)
+library(RSQLite)
+library(RMySQL)
+library(dbplyr)
+
+sqlQuery <- function (query) {
+  # creating DB connection object with RMysql package
+  query = "SELECT * FROM RandomQuestion"
+  DB <- dbConnect(
+    drv = RMySQL::MySQL(),
+    dbname = "student071",
+    host = "esddbinstance-1.ceo4ehzjeeg0.ap-southeast-1.rds.amazonaws.com",
+    username = "student071",
+    password = "YttS3FxB")
+    # close db connection after function call exits
+  on.exit(dbDisconnect(DB))
+    # send Query to obtain result set
+  rs <- dbSendQuery(DB, query)
+    # get elements from result sets and convert to dataframe
+  result <- fetch(rs, -1)
+  killDbConnections <- function () {
+    all_cons <- dbListConnections(MySQL())
+    print(all_cons)
+    for(con in all_cons)
+      +  dbDisconnect(con)
+    print(paste(length(all_cons), " connections killed."))
+  }
+  # return the dataframe
+  return(result)
 }
+
+questionRetrieve <- function(answered_questiCons){
+  #open the connection
+  question =sample(1:35,1, replace=FALSE)
+  data<- sqlQuery("SELECT * FROM RandomQuestion")
+    return(
+      list(
+      "questionID" = question ,
+      "question" = data["question"],
+      "options" =c(data[["a"]],data[["b"]],data[["c"]],data[["d"]]),
+      required = TRUE, # must select one option
+      "correct_option_index" = data["correct_option_index"] #if option 1 is correct, etc
+     # "answered_questions_updated" = c((data["questionID"])) #updated list of answered questions
+      ))
+    #Close the connection
+  dbDisconnect(data)
+}
+
+
+
