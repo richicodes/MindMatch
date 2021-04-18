9c0d489f36d84f0849a728e7d6b7af0c3b8a5930
Chung Wei Lin <weils@Chungs-MacBook-Air.local>
2021-04-18 23:38:30 +0800

    modified functions to reduce lag

--- questions.R
+++ questions.R
@@ -12,48 +12,86 @@ library(RSQLite)
 library(RMySQL)
 library(dbplyr)
 
-sqlQuery <- function (query) {
-  # creating DB connection object with RMysql package
-  query = "SELECT * FROM RandomQuestion"
-  DB <- dbConnect(
+
+
+getAWSConnection <- function(){
+  conn <- dbConnect(
     drv = RMySQL::MySQL(),
     dbname = "student071",
     host = "esddbinstance-1.ceo4ehzjeeg0.ap-southeast-1.rds.amazonaws.com",
     username = "student071",
-    password = "YttS3FxB")
-    # close db connection after function call exits
-  on.exit(dbDisconnect(DB))
-    # send Query to obtain result set
-  rs <- dbSendQuery(DB, query)
-    # get elements from result sets and convert to dataframe
-  result <- fetch(rs, -1)
-  killDbConnections <- function () {
-    all_cons <- dbListConnections(MySQL())
-    print(all_cons)
-    for(con in all_cons)
-      +  dbDisconnect(con)
-    print(paste(length(all_cons), " connections killed."))
+    password = getOption("AWSPassword"))
+  conn
   }
-  # return the dataframe
-  return(result)
-}
 
-questionRetrieve <- function(answered_questiCons){
+# sqlQuery <- function (query) {
+#   # creating DB connection object with RMysql package
+#   conn <- getAWSConnection()
+#   
+#   dbDisconnect(conn)
+#   
+#   DB <- dbConnect(
+#     drv = RMySQL::MySQL(),
+#     dbname = "student071",
+#     host = "esddbinstance-1.ceo4ehzjeeg0.ap-southeast-1.rds.amazonaws.com",
+#     username = "student071",
+#     password = getOption("AWSPassword"))
+#     # close db connection after function call exits
+#   on.exit(dbDisconnect(DB))
+#     # send Query to obtain result set
+#   rs <- dbSendQuery(DB, query)
+#     # get elements from result sets and convert to dataframe
+#   result <- fetch(rs, -1)
+#   killDbConnections <- function () {
+#     all_cons <- dbListConnections(MySQL())
+#     print(all_cons)
+#     for(con in all_cons)
+#       +  dbDisconnect(con)
+#     print(paste(length(all_cons), " connections killed."))
+#   }
+#   # return the dataframe
+#   return(result)
+# }
+
+getQuestions <- function(){
   #open the connection
-  question =sample(1:35,1, replace=FALSE)
-  data<- sqlQuery("SELECT * FROM RandomQuestion")
-    return(
-      list(
-      "questionID" = question ,
-      "question" = data["question"],
-      "options" =c(data[["a"]],data[["b"]],data[["c"]],data[["d"]]),
-      required = TRUE, # must select one option
-      "correct_option_index" = data["correct_option_index"] #if option 1 is correct, etc
-     # "answered_questions_updated" = c((data["questionID"])) #updated list of answered questions
-      ))
-    #Close the connection
-  dbDisconnect(data)
+  conn <- getAWSConnection()
+  query <- "SELECT * FROM GameQuestions"
+  result <- dbGetQuery(conn, query)
+  dbDisconnect(conn)
+  result
 }
 
+
+questionRetrieve <- function(questionsAnswered){
+  questionData <- getQuestions()
+  allQuestions <- c(1:35)
+  
+  questionsNotAnswered <- 0
+  
+  # check if questionsAnswered is null
+  if (is.null(questionsAnswered)){
+    questionsNotAnswered <- allQuestions
+  } else {
+    questionsNotAnswered <- allQuestions[-questionsAnswered]
+  }
+  
+  question <- sample(questionsNotAnswered, 1, replace = FALSE)
+  
+  return(
+    list(
+    "questionID" = question ,
+    "question" = questionData[question,"question"],
+    "options" = c(questionData[question, "a"],
+                 questionData[question, "b"],
+                 questionData[question, "c"], 
+                 questionData[question, "d"]),
+    required = TRUE, # must select one option
+    "correct_option_index" = questionData[question, "correct_option_index"], #if option 1 is correct, etc
+    "answered_questions_updated" = c(questionsAnswered, question) #updated list of answered questions
+    
+    ))
+
+}
 
 
