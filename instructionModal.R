ed32caf5bb02eb09e24d19a546b651ce523b10e9
Ng Ri Chi <65701066+richicodes@users.noreply.github.com>
2021-04-20 02:46:39 +0800

    Modified instructionModal.R

--- instructionModal.R
+++ instructionModal.R
@@ -7,10 +7,10 @@ instructionModal <- function(){
     tags$ul(
       tags$li("Each player takes turns to flip any 2 memory cards within 20 seconds."), 
       tags$li("If the picture on the memory card matches, the player proceed to answer a randomly generated quiz question. Otherwise, the mismatched cards are flipped back and the turn ends. Then, it’s the next player's turn to flip any 2 cards."), 
-      tags$li("If the quiz question is answered correctly, the player collects the 2 matching memory cards and scores a point for each card collected. If answered incorrectly, opponent gets the cards and earns points instead."),
-      tags$li("To obtain a chance card, the player must find the matching coloured memory card."),
+      tags$li("If the quiz question is answered correctly, the player collects the 2 matching memory cards and scores a point for each card collected. If answered incorrectly or if the player chooses to reveal the answer, opponent gets the cards and earns points instead."),
+      tags$li("To obtain an action card, the player must find the matching coloured memory card. Possible action cards are Bonus, Skip or Answer Together."),
       tags$li("If the player chooses matching chance cards, a chance action is randomly selected and it will be effective immediately before the player answers a quiz question."),
-      tags$li("The game ends when all of the memory cards are matched and the player with the most points wins the game.")
+      tags$li("The game ends when all of the memory cards are matched or if the question bank is exhausted. The player with the most points wins the game.")
     )
   ) 
   return(modal)
